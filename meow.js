788d01234aad75e891eb56a46c201f4ecfb873d6
eduardoadoglio <eduardoadoglio4@gmail.com>
2019-09-11 13:55:43 -0300

    Cat generator and counter

--- meow.js
+++ meow.js
@@ -1,4 +1,6 @@
 var request = new XMLHttpRequest();
+var numGatos = 1;
+
 
 request.open('GET', 'http://aws.random.cat/meow', true);
 
@@ -14,6 +16,8 @@ request.send();
 
 
 function newCat(){
+    numGatos++;
+    var counter = document.getElementById("num");
     var request = new XMLHttpRequest();
 
     request.open('GET', 'http://aws.random.cat/meow', true);
@@ -21,9 +25,8 @@ function newCat(){
     request.onload = function(){
         var image = document.getElementById("meow");
         var data = JSON.parse(this.response);
-        console.log(data);
-        console.log(data.file);
         image.src = data.file;
+        counter.innerHTML = numGatos;
     }
 
     request.send();
